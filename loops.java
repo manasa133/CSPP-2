2f01a4fe87aad05bcbc4d5e136152988026157a1
ManasaMona <manasaalisetty@gmail.com>
2018-08-30 17:04:39 +0530

    Iterations session

---
+++ loops.java
@@ -0,0 +1,160 @@
+
+import java.util.Scanner;
+class HelloWorld
+{
+  public static void main(String[] args)
+  {
+  	// int i = 0; 
+  	// if(1<2){
+  	// 	 i = 1;
+  	// }
+  	// System.out.println(i);
+
+  	// int a=10,b=5,c=6;
+  // int i =0;
+  	// for(; ; ){
+  	// 	int j =0;
+  	// 	if(i==10){
+  	// 		break;
+  	// 	}
+  	// 	System.out.println(i);
+  	// 	i++;
+  	// 	}
+
+  	// 	System.out.println(j);
+  	
+  	// }
+
+  // int[] lst = {1,2,3};
+  // 	int j =1;
+  	// for (int i : lst){
+  	// 	if(i==2){
+  	// 		continue;
+  	// 	}
+  	// 	System.out.println(i);
+  
+  	// }
+
+  	// for (int i =0;i<lst.length;i++){
+  	// 	System.out.println(lst[i]);
+  	// }
+
+  	// byte i =256 ;
+  	// int i =0;
+  	// while(true){
+  	// 	System.out.println(i);
+  	// 	i++;
+  	// 	if (i ==10){
+  	// 		break;
+  	// 		System.out.println("hello");
+  	// 	}
+  	// }
+//dowhile loop:
+  	// int i =0;
+  	// Scanner obj =new Scanner(System.in);
+  	// int i=0;
+  	// do{
+  	// 	System.out.println("1. addition, 2. Sunstraction, 3. exit");
+  	// 	 int i = new Scanner(System.in).nextInt();
+  	// 	switch(i){
+  	// 		case 1 :
+  	// 			System.out.println("Addition");
+  	// 			break;
+  	// 		case 2 :
+  	// 			System.out.println("sub");
+  	// 			break;
+  	// 		case 3:
+  	// 			System.out.println("calculation is done Exit");
+  	// 			break;
+  	// 		default :
+  	// 			System.out.println(" only 1 and 2 are accepted");
+  	// 	};
+  
+  	// }while(i<=2);
+  	// while(i<=2){
+  	// 	System.out.println("1. addition, 2. Sunstraction, 3. exit");
+  	 	// String a ="hello";
+  	// int b =3;
+  	//  	 switch(b){
+  	 	 	
+  	// 		case 1:
+  	// 			System.out.println("Addition");
+  	// 		default :
+  	// 			System.out.println(" only 1 and 2 are accepted");
+  	// 		case 2:
+  	// 			System.out.println("sub");
+  	// 			break;
+  			
+  	// 	// }
+ 
+  	// }
+
+// for (int i =0; i<2 ; i++){
+// for (int j =0; j<3; j++)
+// for (int k =0; k<4 ;k ++)
+// System.out.println(i +" "+j+" "+ k);
+// System.out.println( "k is completed");
+// }
+  // 	int  i =2
+		// if(i == 2)
+		// System.out.println("line1");
+		// System.out.println("line3");
+		// if (i ==3)
+		// System.out.println("line2");
+  	// for (int i =0;i<5;i++){
+  	// 	System.out.println(i);
+  	// 	i++;
+  	// }
+  	// int i =0;
+  	// System.out.println(i++);
+  	// System.out.println(i++);
+  	// int j =0;
+  	// System.out.println(++j);
+  	// do{
+  	// 	// System.out.println("i");
+  	// }while(true);
+//   	System.out.println(2);
+// System.out.println(4);
+// System.out.println(6);
+// System.out.println(8);
+// System.out.println(10);
+// System.out.println(GoodBye!);
+  	// int n = 0;
+  	// while(n<10){
+  		
+  	// 	System.out.println(n);
+  	// 	n = n+2;
+  	// }
+  	// System.out.println("GoodBye!");
+  	// Scanner sc = new Scanner(System.in);
+  	// int s = 0;
+  	// int n = sc.nextInt();
+  	// for(int i=1;i<=n;i++){
+  	// 	s = s+i;
+
+  	// }System.out.println(s);
+
+  	for(int i=1;i<=5;i++){
+  		for(int j=1; j<=i; j++){
+  			System.out.print(j +" ");
+  		}
+  		//System.out.println("");
+  	}
+
+  }
+}
+ 
+
+
+
+  		
+  	
+
+
+
+  
+
+
+  
+
+
