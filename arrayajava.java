09582fb121f727f5c07d827e4598839b222771a0
ManasaMona <manasaalisetty@gmail.com>
2018-08-30 17:01:25 +0530

    Arrays Session

---
+++ arrayajava.java
@@ -0,0 +1,82 @@
+import java.util.*;
+class arrayajava {
+	public static void main(String[] args) {
+		// //int[] arr; //declaration
+		// int arr[] ;//declaration
+		
+		// // int size  = new Scanner(System.in).nextInt();
+		// int a=6;
+		// a =7;
+		//   arr = new int[5];//Intialisation
+		//   arr[0] =12;
+		//   arr[3] =14;
+		//   arr[4] =12;
+		//   arr = new int[6];//Intialisation
+		//  // System.out.println(arr[0]+"-"+arr[1]+"-"+arr[3]);
+		//   //String [] arr1 = new String[5];
+		 
+		//   // for (int i =1 ;i <= arr.length ; i++){
+		//   // 		System.out.println(arr[i]+" "+i);
+
+		//   // }
+		//   // for(int i : arr){
+		//   // 	System.out.println(i);
+		//   // }
+
+		//   int[][] arr1;
+		//   int arr2[][];
+		//   int[] arr3[];
+
+		//   arr1 = new int[4][2];
+		//    System.out.println(arr1.length);
+		//   System.out.println(arr1[1].length);
+		//   // [][]int arr4;
+		//  //System.out.println(arr1[0][0]);
+		//   for(int i =0;i<arr1.length;i++){
+		//   	for (int j =0;j<arr1[i].length;j++){
+		//   		System.out.println(arr1[i][j]);
+		//   	}
+		//   }
+
+		 
+		  //System.out.println(arr[0]);
+
+		 // char[] chr =new char[5];
+		 // float[] flt = new float[12];
+		 // String str[] = new char[3][9];
+		int a =6,b=6;
+		// if(a==b){
+		// 	System.out.println("same");
+		// }else{
+		// 	System.out.println("not same");
+		// }
+		// int arr1[] ={1,2,3};
+		// int arr2[] ={1,2,3};
+		// //System.out.println(Arrays.toString(arr1));
+		// if (Arrays.toString(arr1)==(Arrays.toString(arr2))){
+		// 	System.out.println("same");
+		// }else{
+		// 	System.out.println("not same");
+		// }
+		// char s ='a';
+		// int k = 'a';
+		// System.out.println(k);
+		// char h = 45;
+		// System.out.println(h);
+		
+		// int arr1[] ={1,2,3};
+		// arr1[1]=++arr1[1];
+		// System.out.println(Arrays.toString(arr1));
+
+
+		int[] arr[] = new int[3][4];
+		System.out.println(arr.length);
+
+	
+
+
+
+
+	}
+	
+}
