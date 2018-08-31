0756ab7d0f4dae07ce2f33420b114d9ce74b5092
ManasaMona <manasaalisetty@gmail.com>
2018-08-31 12:37:32 +0530

    strings session

---
+++ stringJava.java
@@ -0,0 +1,132 @@
+import java.util.*;
+class stringJava{
+	public static void main(String[] args) {
+		// String s = "helloworld";
+		// System.out.println(s);
+		
+		// char k[] = {'a','b','c'};
+		// String s2 = new String(k);
+		// System.out.println(k); 
+
+		// String s1 = new String("hello");
+		// System.out.println(s1.length());
+
+		// String s2;
+		// System.out.println(s2);
+
+		// String s = "helloworld";
+		// String s1 = "helloworld";
+		// System.out.println(s==s1);
+
+		// String s2 = "helloworld";
+		// String s3 = new String("helloworld");
+		// System.out.println(s2==s3);
+		// System.out.println(s2.equals(s3));
+		// System.out.println(s2.compareTo(s3));
+		// String s4 = "abc";
+		// String s5 = new String("abz");
+		// System.out.println(s4.compareTo(s5));
+		// System.out.println(s5.compareTo(s4));
+
+		//String k1 = "helloworld";
+		//System.out.println(k1[2]);
+		//System.out.println(k1.charAt(2));
+		// for(int i =0;i< k1.length();i++){
+		// 	System.out.print(k1.charAt(i));
+		// }
+
+		// System.out.println(k1.substring(1,4));
+		// System.out.println(k1.substring(1));
+		// //System.out.println(k1.substring(-1));
+		// System.out.println(k1.substring(1,4).substring(0,1));
+
+		// // System.out.println(k1);
+		// String k1 = "helloworld";
+		// System.out.println( k1.indexOf("o"));
+		// System.out.println( k1.indexOf("rl",2));
+		// System.out.println( k1.indexOf("o",5));
+		// System.out.println( k1.indexOf("rd",2));
+
+		//char[] charArr = Arrays.toCharacter(k1);
+		 // String s = "Helloworld";
+		 // String s1 = "helloworld";
+		 // System.out.println(s1.compareTo(s));
+		 // System.out.println(s1.compareToIgnoreCase(s));
+		 // System.out.println(s.concat(s1));
+		 // System.out.println(s1.concat(s));
+		 //  System.out.println(s1.concat(" this "+s));
+
+		// String s = "Helloworld";
+		//  String s1 = "abcdwworls";
+		//  String s3 ="wor";
+		//  // System.out.println(s.contains(s1));
+		 // System.out.println(s.contains(s3));
+		 //  System.out.println(s.endsWith("rld"));
+		 //  System.out.println(s.startsWith("hell"));
+		  // System.out.println(s.isEmpty());
+		  // String s4=" ";
+		  //   System.out.println(s4.isEmpty());
+	//String s = "Helloworld";
+	// System.out.println(s.lastIndexOf("o",5));
+	// System.out.println(s.replace("o","i"));
+	// System.out.println(s.replace("owo","ii"));
+	//String s = "Helloworldowoowo";
+	// System.out.println(s.replace("owo","ii"));
+	// System.out.println(s.replaceFirst("owo","ii"));
+	// System.out.println(s.replaceFirst("owo","ii"));
+	// System.out.println(s.substring(0,1)+"k"+s.substring(2));
+	// System.out.println(Arrays.toString(s.split("")));
+//	String[] temp = s.split("");
+	// String s= "1";
+	// //int k = (int)s;
+	// int k = Integer.parseInt(s);
+	// double k1 = Double.parseDouble(s);
+	//String s = "Helloworldowoowo";
+	// 
+	// for(String c : temp ){
+	// 	System.out.println(c);
+	// }
+		// String s = "Helloworldowoowo";
+		// System.out.println(Arrays.toString(s.toCharArray()));
+		// System.out.println(Arrays.toString(s.split(""))); 
+	// for(char c : s){
+	// 	System.out.println(c);
+	// }
+
+		// int i =1;
+		// String s = Integer.toString(i);
+		// System.out.println(s);
+		// String p = i +"hello";
+		// System.out.println(p);
+		//Scanner sc  = new Scanner(System.in);
+		// //System.out.println(sc.nextLine());
+		// System.out.println(sc.next());
+		// System.out.println(sc.next());
+		// String s = "hello";
+		// //s= s+"i";
+		// s+"i";
+		// System.out.println(s);
+		// StringBuffer sb = new StringBuffer("hello");
+		// sb.append("world");
+		// System.out.println(sb);
+
+		// String s ="abcdefghijklmnopqrstuvwxyz";
+		// System.out.println(s.indexOf("a"));
+		// String input = "helloworld";
+
+		// int[] counter = new int[26]; 
+		// for (int i =0; i< input.length(); i++){
+		// 	char k  = input.charAt(i);//h e
+		// 	int index = s.indexOf(k);//7 5
+		// 	counter[index] =counter[index]+1;//1 
+		// }
+
+		// for (int i = 0;i< counter.length ; i++ ) {
+		// 	System.out.println(i+" "+s.charAt(i) + " "+counter[i]);	
+		//}
+
+		String s  = '"' +"'""'";
+		System.out.println(s);
+	}
+	
+}
---
+++ strpy.py
@@ -0,0 +1,10 @@
+s ="helloworld"
+#s[1] ="p"
+p = "helloworld"
+
+print(p==s)
+s[1:4]
+s[1:]
+print(s[0:0])
+for i in s:
+	print(i)
