79bcca26320e386861ef084d941d893255638fb1
ManasaMona <manasaalisetty@gmail.com>
2018-09-05 17:22:20 +0530

    interfaces

---
+++ interface/List.java
@@ -0,0 +1,15 @@
+class car implements vechile {
+	public static void main(String[] args) {
+		
+	}
+	public void add(int item){
+
+	}
+	public boolean contain(int item){
+		return  true;
+	}
+	public void insert(int item){
+
+	}
+	
+}
---
+++ interface/ListInterface.java
@@ -0,0 +1,8 @@
+public interface vechile{
+	public void wheels();
+	// public void remove(String item);
+	// public int get(int index);
+	// public void indexOf(String item);
+	// public boolean contain(String item);
+
+}
