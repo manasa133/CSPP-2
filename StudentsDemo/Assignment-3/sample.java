a55b819bbdacf95b923d47b55c0733cd750f2b79
ManasaMona <manasaalisetty@gmail.com>
2018-09-06 14:30:10 +0530

    "check_style_score -> 2 of 6 passed. style score: 0/1 "

---
+++ StudentsDemo/Assignment-3/sample.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class sample {
+	public static void main(String[] args) {
+		Object[] list = new Object[10];
+		list[0] = 5;
+		list[1] ="five";
+		System.out.println(Arrays.toString(list)); 
+	}
+}
