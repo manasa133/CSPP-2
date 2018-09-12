dfd21ee350966158cb9771920d133df8d94d0ebd
ManasaMona <manasaalisetty@gmail.com>
2018-09-12 17:06:26 +0530

    "check_style_score -> 1 of 4 passed. style score: 1/1 "

---
+++ module16/StudentDemo/bookyourshow/BookYourShow.java
@@ -0,0 +1,80 @@
+class BookYourShow{
+
+	// int a[] = {1,2,3,5};
+	// String[] b ={ "1","2"};
+
+	//Show s1 = new Show("mov1","dattime1",b};
+	// Show s2 = new Show("mov2","dattime2",b};
+
+
+	// Show[] showsArray = {s1,s2 };
+
+	Show[] showsArray;
+	int size;
+	BookYourShow(){
+		showsArray = new Show[10];
+		size = 0;
+	}
+	public void addAShow(Show var){
+		showsArray[size++]=var;
+	}
+
+	public Show getAShow( String moviename, String dattime){
+		for(Show s : showsArray){
+			if(s!=null){
+			if(s.movieName.equals(moviename) && s.dateTime.equals(dattime)){
+				return s;
+			}
+		}
+		}
+		return null;
+	}
+
+	public void bookAShow( String moviename, String dattime, Patron p, String[] bookingSeats){
+		for(Show s : showsArray){
+			if(s!=null){
+			if(s.movieName.equals(moviename) && s.dateTime.equals(dattime)){
+				for(String seatnum : bookingSeats){
+					for(int i = 0; i< s.seatNums.length;i++){
+						if(s.seatNums[i].equals(seatnum)){
+							s.seatNums[i] = "N/A";
+							s.bookedSeats[i] = p;
+						}
+					}
+				}
+			}
+		}}
+	}
+	public void printTicket(String moviename,String dattime,String mobNum){
+		boolean booked = false;
+		for(Show s : showsArray){
+			if(s!=null){
+			if(s.movieName.equals(moviename) && s.dateTime.equals(dattime)){
+				for(int i =0 ; i< s.bookedSeats.length; i++){
+					if(s.bookedSeats[i]!=null){
+					if(s.bookedSeats[i].mobileNumber.equals(mobNum)){
+						booked = true;
+					}
+				}
+
+				}
+			}
+		}
+		}
+		if(booked){
+			System.out.println(mobNum +" "+moviename +" "+ dattime);
+		}
+
+	}
+
+	public void showAll(){
+		for(Show s: showsArray){
+			if(s!=null){
+
+			}
+
+		}
+
+
+	}
+}
---
+++ module16/StudentDemo/bookyourshow/Show.java
@@ -0,0 +1,14 @@
+class Show{
+	String movieName;
+	String dateTime;
+	String[] seatNums;
+	Patron[] bookedSeats;
+	Show(String name,String dateTime, String[] seats){
+		this.movieName = name;
+		this.dateTime = dateTime;
+		this.seatNums = seats;
+		this.bookedSeats = new Patron[seats.length];
+	}
+
+
+}
