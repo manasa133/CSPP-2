62df272af1bcd5c777acd78f59b933646f0a3abb
ManasaMona <manasaalisetty@gmail.com>
2018-09-12 11:23:18 +0530

    "check_style_score -> 0 of 4 passed. style score: 1/1 "

--- module16/mySolution/bookyourshow/BookYourShow.java
+++ module16/mySolution/bookyourshow/BookYourShow.java
@@ -1,27 +1,37 @@
 class BookYourShow{
-	Show[] shows = new Show[10];
+	Show[] shows;
 	int count = 0;
+
+	BookYourShow(){
+		shows = new Show[10];
+		count = 0;
+	}
 
 	public void addAShow(Show s){
 		shows[count++] =  s;
+		//System.out.println(shows[count-1]);
 
 	}
 	public Show getAShow(String name, String date){
 		for(Show s : shows){
-			if(s.movieName == name && s.movieTime.equals(date)){
-				return s;
+			if(s!=null){
+				if(s.movieName.equals(name) && s.movieTime.equals(date)){
+					return s;
+				}
 			}
 		}
 		return null;
 	}
 	public void bookAShow(String movie,String time,Patron p,String[] seats ){
 		for(Show s : shows){
-			if(s.movieName.equals(movie) && s.movieTime.equals(time)){
-				for(String each : seats){
-					for(int i = 0; i< s.seatNumbers.length ; i++){
-						if(each.equals(s.seatNumbers[i])){
-							if(s.bookedSeats[i]==null){
-								s.bookedSeats[i] = p;
+			if(s!=null){
+				if(s.movieName.equals(movie) && s.movieTime.equals(time)){
+					for(String each : seats){
+						for(int i = 0; i< s.seatNumbers.length ; i++){
+							if(each.equals(s.seatNumbers[i])){
+								if(s.bookedSeats[i]==null){
+									s.bookedSeats[i] = p;
+								}
 							}
 						}
 					}
